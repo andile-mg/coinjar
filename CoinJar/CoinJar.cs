e5afc6dd5e7c5ba30beda3777f02fdfc01540b56
Andile Gumede <44235796+andile-mg@users.noreply.github.com>
2021-06-11 11:25:24 +0200

    Add files via upload

---
+++ CoinJar/CoinJar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoinJar
+{
+    public class CoinJar : ICoinJar
+    {
+        private List<ICoin> Coins { get; set; }
+
+        public decimal TotalVolume { get; private set; }
+
+        public decimal UsedVolume { get; private set; }
+
+        public decimal TotalAmount { get; private set; }
+
+        public CoinJar()
+        {
+            Coins = new List<ICoin>();
+            TotalAmount = 0m;
+            TotalVolume = 32m;
+            UsedVolume = 0m;
+        }
+
+        public void AddCoin(ICoin coin)
+        {
+            if (coin == null || coin.Mint != typeof(USAMint))
+            {
+                return;
+            }
+
+            if (coin.Volume + UsedVolume > TotalVolume)
+            {
+                return;
+            }
+
+            Coins.Add(coin);
+            TotalAmount += coin.Amount;
+            UsedVolume += coin.Volume;
+        }
+
+        public decimal GetTotalAmount()
+        {
+            return TotalAmount;
+        }
+
+        public void Reset()
+        {
+            List<ICoin> coins = Coins;
+            Coins = new List<ICoin>();
+            TotalAmount = 0m;
+            UsedVolume = 0m;
+        }
+    }
+}
